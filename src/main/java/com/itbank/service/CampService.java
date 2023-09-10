006db11a0a0bf191ea38cebf26cd74d4235a1950
ojm0827 <127277428+ojm0827@users.noreply.github.com>
2023-09-11 08:59:50 +0900

    Update CampService.java

--- src/main/java/com/itbank/service/CampService.java
+++ src/main/java/com/itbank/service/CampService.java
@@ -26,7 +26,7 @@ public class CampService {
 	@Autowired
 	private CampDAO campDAO;
 
-	private File dir = new File("/usr/local/tomcat/webapps/upload");
+	private File dir = new File("/usr/local/tomcat/webapps/upload/camp");
 
 	
 	public int selectCnt(SearchDTO search) {
